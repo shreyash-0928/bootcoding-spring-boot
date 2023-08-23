6c05176dce0e9341a1722d64950fa600e463902c
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-08-23 22:22:43 +0530

    Coupon System Using Spring

--- coupon-system/src/main/java/com/couponsystem/model/Coupon.java
+++ coupon-system/src/main/java/com/couponsystem/model/Coupon.java
@@ -1,15 +1,35 @@
 package com.couponsystem.model;
 
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@Table(name = "Coupon_app")
+@Entity
 
 // All properties should be private
 // Getter and Setters By default public rehte h ------> @Data(Annotation).
 public class Coupon {
+    @Id
+    @GeneratedValue
     private String id;
+    @Column(name = "Title")
+    private String title;
+    @Column(name = "Type")
     private String type;
+    @Column(name = "Validity")
     private int validFor;
+    @Column(name = "Description")
+    private String description;
+    @Column(name = "Coupon_Code")
+    private String couponCode;
+    @Column(name = "Category")
+    private String category;
+    @Column(name = "Created_date")
+    private String createdDate;
+    @Column(name = "Active_date")
+    private String inActive;
+
 }
