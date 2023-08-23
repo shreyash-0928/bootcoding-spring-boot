ab89acee15b2631a786dac284382e44fa600849d
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-08-23 22:24:37 +0530

    Coupon System Using Spring

--- coupon-system/src/main/java/com/couponsystem/model/User.java
+++ coupon-system/src/main/java/com/couponsystem/model/User.java
@@ -1,14 +1,28 @@
 package com.couponsystem.model;
+
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@Table(name = "app_user")
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
     private int id;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name =  "email_id")
     private String emailId;
+
+    @Column(name = "phone")
     private long phone;
 
 }
