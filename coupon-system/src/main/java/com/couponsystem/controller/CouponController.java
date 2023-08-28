a121c7b4a5600c0d125d52157ad92c663b2170e9
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-08-28 20:15:01 +0530

    Coupon System Using Spring

--- coupon-system/src/main/java/com/couponsystem/controller/CouponController.java
+++ coupon-system/src/main/java/com/couponsystem/controller/CouponController.java
@@ -15,11 +15,11 @@ public class CouponController {
     private CouponService couponService;
 
     // 1. Get All Coupons
-    @GetMapping("all-coupons")
-    public List<String> getAllCoupons(){
-        int quantity = 10;
-        return couponService.getNewCoupon(quantity);
-    }
+//    @GetMapping("all-coupons")
+//    public List<String> getAllCoupons(){
+//        int quantity = 10;
+//        return couponService.getNewCoupon(quantity);
+//    }
 
     // 2. Get Coupon Details by Coupon Id using PathVariable
 //    @GetMapping("coupon-id/{id}")
@@ -28,48 +28,46 @@ public class CouponController {
 //    }
 
     // 2.2 Get Coupon Details by Coupon Id using PathVariable
-    @GetMapping("coupon-id/{id}")
-    public Coupon getCouponObjectById(@PathVariable("id") String couponId){
-        Coupon coupon = couponService.generateNewCoupon();
-        return coupon;
-    }
+//    @GetMapping("coupon-id/{id}")
+//    public Coupon getCouponObjectById(@PathVariable("id") String couponId){
+//        Coupon coupon = couponService.generateNewCoupon();
+//        return coupon;
+//    }
 
-    @GetMapping("generate/new")
-    public Coupon generateNewCoupon(){
-        Coupon coupon = couponService.generateNewCoupon();
-        return coupon;
-    }
-    // 3. Get Coupons with Filter (Query Parameter)
-    @GetMapping("search_coupon")
-    public String searchCoupon(
-            @RequestParam("valid") boolean isValid,
-            @RequestParam(value = "type", required = false) String couponType,
-            @RequestParam("category") String category){
-        return "Query Parameter valid = " + isValid
-                + " | Coupon Type = " + couponType
-                + " | Coupon Category = " + category;
-    }
+//    @GetMapping("generate/new")
+//    public Coupon generateNewCoupon(){
+//        Coupon coupon = couponService.generateNewCoupon();
+//        return coupon;
+//    }
+////    // 3. Get Coupons with Filter (Query Parameter)
+//    @GetMapping("search_coupon")
+//    public String searchCoupon(
+//            @RequestParam("valid") boolean isValid,
+//            @RequestParam(value = "type", required = false) String couponType,
+//            @RequestParam("category") String category){
+//        return "Query Parameter valid = " + isValid
+//                + " | Coupon Type = " + couponType
+//                + " | Coupon Category = " + category;
+//    }
     // HTTP Methods Get
-    @GetMapping("getCoupon")
-    public String newCoupon(){
-        System.out.println("/getCoupon invoked");
-        return "New Coupon : " + couponService.newCoupon();
-    }
+//    @GetMapping("getCoupon")
+//    public String newCoupon(){
+//        System.out.println("/getCoupon invoked");
+//        return "New Coupon : " + couponService.newCoupon();
+//    }
+//
+//    @GetMapping("getJson")
+//    public Coupon getDummyJson(){
+//        System.out.println("/getJson invoked");
+//        return Coupon.builder()
+//                .id(couponService.newCoupon())
+//                .type("Voucher")
+//                .validFor(3).build();
+//    }
 
-    @GetMapping("getJson")
-    public Coupon getDummyJson(){
-        System.out.println("/getJson invoked");
-        return Coupon.builder()
-                .id(couponService.newCoupon())
-                .type("Voucher")
-                .validFor(3).build();
-    }
-
-    @GetMapping("getMultipleCoupons")
-    public List<String> newCouponWithQuantity(){
-        System.out.println("/getMultipleCoupons invoked");
-        int quantity = 10;
-        return couponService.getNewCoupon(quantity);
+    @PostMapping("/couponDetails/{value}")
+    public List<Coupon> generateNewCoupon(@PathVariable int value ) {
+        return couponService.generateNewCoupon(value);
     }
     // http://localhost:8082/app-name/coupon/getCoupon
 }
