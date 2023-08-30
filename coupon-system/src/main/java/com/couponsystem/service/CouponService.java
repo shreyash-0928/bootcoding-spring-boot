0b5c5f0f43e7653413e465cfe36c818c3e7574df
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-08-30 21:54:16 +0530

    Coupon System Using Spring

--- coupon-system/src/main/java/com/couponsystem/service/CouponService.java
+++ coupon-system/src/main/java/com/couponsystem/service/CouponService.java
@@ -2,44 +2,16 @@ package com.couponsystem.service;
 
 import com.couponsystem.model.Coupon;
 import com.couponsystem.model.User;
+import com.couponsystem.repository.CouponRepository;
+import com.couponsystem.utils.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 @Service
 public class CouponService {
-
-    // To Write Business Logic
-    @Autowired
-    private CouponHelper couponHelper;
-
-    public CouponService(CouponHelper couponHelper){
-        this.couponHelper = couponHelper;
-    }
-
-    public String newCoupon(){
-        UUID uuid = UUID.randomUUID();
-        return uuid.toString();
-    }
-    public List<String> getNewCoupon(int quantity){
-        List<String> coupons = new ArrayList<>();
-        for(int i = 0; i < quantity; i++){
-            String newCoupon = newCoupon();
-            coupons.add(newCoupon);
-        }
-        return coupons;
-
-    }
-
-    public Coupon generateNewCoupon() {
-        Coupon coupon = Coupon.builder()
-                .couponCode(UUID.randomUUID().toString())
-                .build();
-        return coupon;
-    }
 
     public boolean save(User user) {
         users.add(user);
@@ -62,5 +34,30 @@ public class CouponService {
             }
         }
         return null;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+
+    @Autowired
+    private CouponRepository couponRepository;
+    public List<Coupon> generateNewCoupon(int value) {
+        List<Coupon>list= new ArrayList<>();
+        for (int i = 0; i < value; i++) {
+            Coupon coupon = Coupon.builder().title(CouponTitle.generateTitle())
+                    .description(CouponDescription.GenerateDescription())
+                    .couponCode(CouponCode.generateCoupon())
+                    .validDate(ValidityDate.generateDate())
+                    .type(CouponType.type())
+                    .discount(CouponDiscount.discount())
+                    .category(CouponCategory.category())
+                    .createBy(CreatedBy.createdBy())
+                    .status(CouponStatus.generateStatus())
+                    .build();
+            list.add(coupon);
+        }
+        return couponRepository.saveAll(list);
     }
 }
